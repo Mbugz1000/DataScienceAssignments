2fe54f8911c1e4ee429465e50eeed6ea2768392b
KTMBUGUA <km.thairu@gmail.com>
2019-01-31 15:36:36 +0300

    After Numpy Lesson

---
+++ Lists.py
@@ -0,0 +1,29 @@
+import numpy as np
+# Lists class
+
+# fruits = list(('Apple', 'Banana', 'Carrot', 'Tomato', 'Treetomato'))
+# no = 0
+# for x in enumerate(fruits):
+#     print(x)
+
+x = [1,2,3,4]
+y = [5,6,7,8,9]
+y1 = x+y
+
+# Using list comprehension to raise it to ^3
+z = [y1**3 for y1 in y1]
+# print(z)
+
+a = [[1,2],['Kaka','Dida'],[10,12]]
+# print(a[2][1])
+
+b = np.array([1,2,3,4,5])
+# print(b)
+
+c = np.array([[2,1],
+              [1,0]])
+
+d = np.array([[4],
+              [1]])
+
+print(c.dot(d), d.shape)
--- NumpyQuiz.py
+++ NumpyQuiz.py
@@ -1,11 +1,14 @@
-import numpy as i
+import numpy as np
 some_list = [1, 2, 3, 5, 7, 9]
-
+#
 mylist = []
 
 for i in some_list:
-    if not i.isnan():
+    if not np.isnan(i):
         mylist.append(i)
 
-#List Comprehension
-#myList2=[i for ]
+# List Comprehension
+myList2 = [i for i in mylist if not np.isnan(i)]
+print(myList2)
+
+print(np.version.version)
