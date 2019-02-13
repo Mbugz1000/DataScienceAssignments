80d1d2516c82ec594ccf4521ef028e1e33f68646
KTMBUGUA <km.thairu@gmail.com>
2019-02-13 11:52:59 +0300

    Did all of the visualisations desired. Sorting stacked bars still an issue.

--- MiniProject.py
+++ MiniProject.py
@@ -49,37 +49,7 @@ df['sector'] = sectors.str.capitalize()
 # if __name__ == "__main__" : main()
 
 import random
-
-def linegraphfunction(dataplot, title, xlabel, ylabel, num):
-    r = lambda: random.randint(0, 255)
-    setStyles = ['-.', '--', '-', ':', 'o', '+']
-
-    plt.figure()
-    for i in range(0, num):
-        plt.plot(years, dataplot.iloc[i], setStyles[i % 6], color='#%02X%02X%02X' % (r(), r(), r()),
-                 label=dataplot.index[i])
-
-    plt.legend(loc="upper left")
-
-    plt.title(title, fontsize=16, fontweight='bold')
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-
-    plt.show()
-
-# Grouped by County (Top 5, Lowest 5)
-years = ['f2003_2004', 'f2004_2005', 'f2005_2006', 'f2006_2007', 'f2007_2008', 'f2008_2009', 'f2009_2010']
-countyPlot = df.groupby(['county']).sum().sort_values('total_amount', ascending=False)[years]
-
-linegraphfunction(countyPlot, "Yearly Costs Per County", "Years", "Total Funds", 8)
-
-# Grouped by Sector
-sectorPlot = df.groupby(['sector']).sum().sort_values('total_amount', ascending=False)[years]
-
-linegraphfunction(sectorPlot, "Yearly Costs Per Sector", "Years", "Total Funds", 8)
-
-
-def barchartfunction(mainDataFrame, groups, subgroups, num, xaxis):
+def barchartfunction(mainDataFrame, groups, subgroups, num, xaxis,title):
     r = lambda: random.randint(0, 255)
     color = '#%02X%02X%02X' % (r(), r(), r())
 
@@ -101,27 +71,73 @@ def barchartfunction(mainDataFrame, groups, subgroups, num, xaxis):
     r1 = np.arange(num)
     plt.figure()
     for i, bars in enumerate(outerList):
-        plt.bar(r1, bars[0:num], color='#%02X%02X%02X' % (r(), r(), r()), width=barWidth, edgecolor='white',label=subgroups[i])
+        plt.bar(r1, bars[0:num], color='#%02X%02X%02X' % (r(), r(), r()), width=barWidth, edgecolor='white', label=subgroups[i])
         r1 = [x + barWidth for x in r1]
 
     # Add xticks on the middle of the group bars
     plt.xlabel(xaxis, fontweight='bold')
-    plt.xticks([r2 + barWidth for r2 in range(num)], groups)
+    plt.xticks([r2 + barWidth for r2 in range(num)], groups, rotation=45)
+    plt.title(title, fontsize=16, fontweight='bold')
 
     # Create legend & Show graphic
     plt.legend()
     plt.show()
+def linegraphfunction(dataplot, title, xlabel, ylabel, num):
+    r = lambda: random.randint(0, 255)
+    setStyles = ['-.', '--', '-', ':', 'o', '+']
+
+    plt.figure()
+    for i in range(0, num):
+        plt.plot(years, dataplot.iloc[i], setStyles[i % 6], color='#%02X%02X%02X' % (r(), r(), r()),
+                 label=dataplot.index[i])
+
+    plt.legend(loc="upper left")
+
+    plt.title(title, fontsize=16, fontweight='bold')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    plt.show()
+
+
+# Grouped by County (Top 5, Lowest 5)
+years = ['f2003_2004', 'f2004_2005', 'f2005_2006', 'f2006_2007', 'f2007_2008', 'f2008_2009', 'f2009_2010']
+countyPlot = df.groupby(['county']).sum().sort_values('total_amount', ascending=False)[years]
+linegraphfunction(countyPlot, "Yearly Costs for Top 10 Counties", "Years", "Total Funds", 10)
+
+# Grouped by Sector
+sectorPlot = df.groupby(['sector']).sum().sort_values('total_amount', ascending=False)[years]
+linegraphfunction(sectorPlot, "Yearly Costs for Top 10 Sectors", "Years", "Total Funds", 10)
 
 # Group by Completion per Sector
 groups = df['sector'].unique()
 subgroups = df['implementation_status'].unique()
 completionSector = df.groupby(['sector', 'implementation_status']).sum().sort_values('total_amount', ascending=False)['total_amount']
+barchartfunction(completionSector, groups, subgroups, 8, 'Sectors', 'Top 8 Sectors by Expenditure')
+completionSector2 = df.groupby(['sector', 'implementation_status']).sum().total_amount.unstack().plot.bar(stacked=True)
+plt.title('Sector Expenditure by Implementation Status', fontsize=16, fontweight='bold')
 
-barchartfunction(completionSector, groups, subgroups, 8, 'Sectors')
-
-# Group by Total Amount/ Estimated Amount per Sector
+# Group by Completion per County
 countyGroups = df['county'].unique()
-countySector = df.groupby(['county', 'implementation_status']).sum().sort_values('total_amount', ascending=False)['total_amount']
+completionCounty = df.groupby(['county', 'implementation_status']).sum().sort_values('total_amount', ascending=False)['total_amount']
+barchartfunction(completionCounty, countyGroups, subgroups, 15, 'Counties', 'Top 15 Counties by Expenditure')
+countySector2 = df.groupby(['county', 'implementation_status']).sum().total_amount.unstack().plot.bar(stacked=True)
+plt.title('County Expenditure by Implementation Status', fontsize=16, fontweight='bold')
 
-barchartfunction(countySector, countyGroups, subgroups, 8, 'Counties')
+# No of Projects per County
+plt.figure()
+df.groupby('county')['objectid'].count().sort_values(ascending=False).plot.bar()
+plt.title('No. of Projects Per County', fontsize=16, fontweight='bold')
 
+# No of Projects per Sector
+plt.figure()
+df.groupby('sector')['objectid'].count().sort_values(ascending=False).plot.bar()
+plt.title('No. of Projects Per Sector', fontsize=16, fontweight='bold')
+
+# Estimated Output/ Actual Totals per Sector
+df.groupby('sector')['estimated_cost', 'total_amount'].sum().sort_values('estimated_cost', ascending=False).plot.bar()
+plt.title('Estimates Output vs Actual Totals per Sector', fontsize=16, fontweight='bold')
+
+# Estimated Output/ Actual Totals per County
+df.groupby('county')['estimated_cost', 'total_amount'].sum().sort_values('total_amount', ascending=False).plot.bar()
+plt.title('Estimates Output vs Actual Totals per County', fontsize=16, fontweight='bold')
